b2b6763f8090338076be0bfd55b571392a8a86e8
davigopi <davigopi@gmail.com>
2026-08-24 13:35:07 -0300

    davigopi 2026-08-24 13:35:06 update

--- manip_desktop_dgp.py
+++ manip_desktop_dgp.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import pyautogui
+from PIL import ImageGrab
 import cv2
 from mss import MSS
 import os
@@ -20,7 +21,7 @@ def print_padao(texto_1=None, texto_2=None, titulo=None, rodape=None):
     if salvar_imagens:
         if texto_1 and "_|" in texto_1:
             palv_1, palv_2 = texto_1.split("_|")
-            texto_1 = f'{palv_1:.<14} | {palv_2:.<23}'
+            texto_1 = f'\n{palv_1:.<14} | {palv_2:.<23}'
         if titulo:
             print(f'\n{50*"#"}\n{titulo} \n{50*"_"}')
         elif rodape:
@@ -37,14 +38,14 @@ def resource_path(relative_path):
         path_sys = Path.cwd()
     return path_sys / relative_path
 
-def salve_tela(tela, nome_arq):
+def salve_img(img, nome_arq):
     global salvar_imagens
-    if salvar_imagens and tela is not None and tela.size > 0 and nome_arq:
+    if salvar_imagens and img is not None and img.size > 0 and nome_arq:
         arq_sist = nome_arq + '.png'
         arq = os.path.join('img', 'capturadas', arq_sist)
-        path_tela = resource_path(arq)
-        os.makedirs(os.path.dirname(path_tela), exist_ok=True)
-        cv2.imwrite(path_tela, tela)
+        path_img = resource_path(arq)
+        os.makedirs(os.path.dirname(path_img), exist_ok=True)
+        cv2.imwrite(path_img, img)
 
 def clk_x_y(x, y):
     pyautogui.click(x, y)
@@ -69,25 +70,19 @@ def setup_tesseract():
         print_padao(texto_1=f"Versão do Tesseract: {pytesseract.get_tesseract_version()}")
         print_padao(rodape=True)
 
-def define_region():
-    target_monitor = get_monitor(width=1360, height=768)
-    left = target_monitor["left"]
-    top = target_monitor["top"]
-    width = target_monitor["width"]
-    height = target_monitor["height"]
-    top_offset = int(height * 0.16)
-    bottom_offset = int(height * 0.06)
+def get_monitor(width=None, height=None, index_nonitor=1):
+    target_monitor = get_monitor_by_size(width, height) if width and height else get_monitor_by_index(index_nonitor)
     region = {
-        "left": left,                                   # X
-        "top": top + top_offset,                        # Y
-        "width": width - 4,                             # largura
-        "height": height - top_offset - bottom_offset   # altura
+        "left": int(target_monitor["left"]),                                                                                    # X
+        "top": int(target_monitor["top"]) + int(target_monitor["height"] * 0.16),                                               # Y
+        "width": int(target_monitor["width"]) - 4,                                                                              # largura
+        "height": int(target_monitor["height"]) - int(target_monitor["height"] * 0.16) - int(target_monitor["height"] * 0.06)   # altura
     }
-    region_list = [region["left"], region["top"], region["width"], region["height"]]
-    return region, region_list
+    region["list"] = [int(region["left"]), int(region["top"]), int(region["width"]), int(region["height"])]
+    return region
 
-def get_monitor(width=1360, height=768):
-  # with garante que os instância do capturador de tela da biblioteca mss sejam liberados de forma correta ao finalizar o bloco.
+def get_monitor_by_size(width, height):
+  # with garante que os instância do capturador de img da biblioteca mss sejam liberados de forma correta ao finalizar o bloco.
   with MSS() as sct:
     monitores = sct.monitors[1:]
     for monitor in monitores:
@@ -97,51 +92,100 @@ def get_monitor(width=1360, height=768):
       return monitores[1]
     return monitores[0]
 
-def get_tela(region):
-    # Captura a região da tela via MSS e padroniza a conversão para BGR.
-    with MSS() as sct:
-        region = region if region else sct.regions[1]
-        sct_img = sct.grab(region)
-        tela = np.array(sct_img)
-        tela_bgr = cv2.cvtColor(tela, cv2.COLOR_BGRA2BGR)
-        return tela_bgr
-    raise Exception("Não existe Tela")
+def get_monitor_by_index(index_nonitor):
+    with mss() as sct:
+        if 1 <= index_nonitor < len(sct.monitors):
+            return sct.monitors[index_nonitor]
+        return sct.monitors[1]
 
-def get_tela_all(scale, region):
+def get_img_mss(region):
+    # Captura a região da img via MSS e padroniza a conversão para BGR.
+    with MSS() as sct:
+        region = region if region else sct.monitors[1]
+        sct_img = sct.grab(region)
+        img = np.array(sct_img)
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+        # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+        return img_bgr
+    raise Exception("Não existe img")
+
+def get_img_pil(region):
+    # sct_img = pyautogui.screenshot(region=region['list'])
+    # img = np.array(sct_img)
+    # img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # return img_bgr
+
+    # 1. Converte a região (left, top, width, height) para a bounding box (left, top, right, bottom)
+    left = region["left"]
+    top = region["top"]
+    right = left + region["width"]
+    bottom = top + region["height"]
+
+    # 2. Captura a tela usando o Pillow com suporte a múltiplos monitores (coordenadas negativas)
+    sct_img = ImageGrab.grab(bbox=(left, top, right, bottom), all_screens=True)
+
+    # 3. Converte para array NumPy e ajusta a cor de RGB para BGR (OpenCV)
+    img = np.array(sct_img)
+    img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    return img_bgr
+
+def get_all_img(region):
     dict_arqs = {}
-    dict_arqs['tela'] = get_tela(region)
-    dict_arqs['tela_cv'] = cv2.cvtColor(dict_arqs['tela'], cv2.COLOR_BGRA2BGR)
-    dict_arqs['gray'] = cv2.cvtColor(dict_arqs['tela'], cv2.COLOR_BGRA2GRAY)
+    if region['get_img'] == 'pil':
+        dict_arqs['img'] = get_img_pil(region)
+        dict_arqs['img_cv'] = cv2.cvtColor(dict_arqs['img'], cv2.COLOR_RGB2BGR)
+        dict_arqs['gray'] = cv2.cvtColor(dict_arqs['img'], cv2.COLOR_RGB2GRAY)
+    else:
+        dict_arqs['img'] = get_img_mss(region)
+        dict_arqs['img_cv'] = cv2.cvtColor(dict_arqs['img'], cv2.COLOR_BGRA2BGR)
+        dict_arqs['gray'] = cv2.cvtColor(dict_arqs['img'], cv2.COLOR_BGRA2GRAY)
+    # dict_arqs['img_cv'] = cv2.cvtColor(dict_arqs['img'], cv2.COLOR_BGRA2BGR)
+    # dict_arqs['gray'] = cv2.cvtColor(dict_arqs['img'], cv2.COLOR_BGRA2GRAY)
+    scale = region.get('scale', 2)
     dict_arqs['grayClr'] = cv2.resize(dict_arqs['gray'], None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+
+    # ===> NOVO: CLAHE para destacar o texto cinza em fundo escuro no MSS <===
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    dict_arqs['grayClahe'] = clahe.apply(dict_arqs['grayClr'])
+
     dict_arqs['grayEsc'] = cv2.equalizeHist(dict_arqs['grayClr'])
     dict_arqs['grayEsc'] = cv2.GaussianBlur(dict_arqs['grayEsc'], (3, 3), 0)
+
     img_inv = cv2.bitwise_not(dict_arqs['grayClr'])
     dict_arqs['contrClr'] = cv2.addWeighted(img_inv, 1.5, np.zeros(img_inv.shape, img_inv.dtype), 0, 0)
     dict_arqs['contrClr3'] = cv2.addWeighted(img_inv, 3.0, np.zeros(img_inv.shape, img_inv.dtype), 0, 0)
     dict_arqs['contrClr6'] = cv2.addWeighted(img_inv, 6.0, np.zeros(img_inv.shape, img_inv.dtype), 0, 0)
+
     img_inv = cv2.bitwise_not(dict_arqs['grayEsc'])
     dict_arqs['contrEsc'] = cv2.addWeighted(img_inv, 1.5, np.zeros(img_inv.shape, img_inv.dtype), 0, 0)
     dict_arqs['contrEsc3'] = cv2.addWeighted(img_inv, 3.0, np.zeros(img_inv.shape, img_inv.dtype), 0, 0)
     dict_arqs['contrEsc6'] = cv2.addWeighted(img_inv, 6.0, np.zeros(img_inv.shape, img_inv.dtype), 0, 0)
+
     _, dict_arqs['threshClr'] = cv2.threshold(dict_arqs['grayClr'], 150, 255, cv2.THRESH_BINARY)
     _, dict_arqs['threshEsc'] = cv2.threshold(dict_arqs['grayEsc'], 150, 255, cv2.THRESH_BINARY)
+
+    dict_arqs['threshAdapt'] = cv2.adaptiveThreshold(
+        dict_arqs['grayClahe'], 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
+    )
+
     dict_arqs['threshInvClr'] = cv2.bitwise_not(dict_arqs['threshClr'])
     dict_arqs['threshInvEsc'] = cv2.bitwise_not(dict_arqs['threshEsc'])
     _, dict_arqs['mask'] = cv2.threshold(dict_arqs['contrClr3'], 150, 255, cv2.THRESH_BINARY)
     return dict_arqs
 
-def get_tela_img(region):
+def get_dict_img(region):
     dict_arqs = {}
-    dict_arqs['img_tela_bgr'] = get_tela(region)
-    dict_arqs['img_tela_gray'] = cv2.cvtColor(dict_arqs['img_tela_bgr'], cv2.COLOR_BGR2GRAY)
+    dict_arqs['img_bgr'] = get_img_mss(region)
+    dict_arqs['img_gray'] = cv2.cvtColor(dict_arqs['img_bgr'], cv2.COLOR_BGR2GRAY)
     return dict_arqs
 
-def set_imgs_find(palv, fator, porcentagem) :
+def find_dif_size_img(palv, fator, porcentagem) :
     dict_arqs_find = {}
     arq = resource_path(os.path.join('img', palv))
     img_find = cv2.imread(arq, cv2.IMREAD_GRAYSCALE)
     if img_find is None or img_find.size == 0:
-        print_padao(texto_1=f"Erro ao carregar tela: {img}")
+        print_padao(texto_1=f"Erro ao carregar img: {img}")
         raise Exception("Não existe imagem para procurar")
     list_zoom = [1]
     for i in range(fator, porcentagem+1, fator):
@@ -152,58 +196,53 @@ def set_imgs_find(palv, fator, porcentagem) :
     return dict_arqs_find
 
 class ImageManip:
-    def __init__(self, *args, **kwargs) -> None:
-        self.palv = kwargs.get("palv")
-        self.reduce_confidence = kwargs.get("reduce_confidence", 0.05)
-        self.metodo = "mss_image"
+    def locate_x_y(self, palv, reduce_confidence=0.05, metodo="mss_image", width=None, height=None, index_nonitor=1):
         setup_tesseract()
-    @property
-    def locate_x_y(self):
         confidence = 0.95
         confidence_print = 1
         confidence_minima = 0.65
         count = 1
         n_tentativas = 2
-        region, region_list = define_region()
-        if not self.palv:
-            raise Exception(f'Não informado palavra da imagem {self.palv}.')
+        region = get_monitor(width, height, index_nonitor)
+        if not palv:
+            raise Exception(f'Não informado palavra da imagem {palv}.')
         while True:
             try:
-                if self.metodo == "mss_image":
-                    dict_arqs = get_tela_img(region)
+                if metodo == "mss_image":
+                    dict_arqs = get_dict_img(region)
                     for nome_arq in dict_arqs:
-                        salve_tela(dict_arqs[nome_arq], nome_arq)
-                    dict_arqs_find = set_imgs_find(self.palv, 2, 30)
+                        salve_img(dict_arqs[nome_arq], nome_arq)
+                    dict_arqs_find = find_dif_size_img(palv, 2, 30)
                     for palv, arq_find in dict_arqs_find.items():
-                        # Evita erro caso a imagem procurada seja maior que a região da tela
-                        if arq_find.shape[0] > dict_arqs['img_tela_gray'].shape[0] or \
-                        arq_find.shape[1] > dict_arqs['img_tela_gray'].shape[1]:
+                        # Evita erro caso a imagem procurada seja maior que a região da img
+                        if arq_find.shape[0] > dict_arqs['img_gray'].shape[0] or \
+                        arq_find.shape[1] > dict_arqs['img_gray'].shape[1]:
                             continue
-                        res = cv2.matchTemplate(dict_arqs['img_tela_gray'], arq_find, cv2.TM_CCOEFF_NORMED)
+                        res = cv2.matchTemplate(dict_arqs['img_gray'], arq_find, cv2.TM_CCOEFF_NORMED)
                         locais = np.where(res >= confidence)
                         if len(locais[0]) > 0:
                             x, y = locais[1][0], locais[0][0]
                             h, w = arq_find.shape[:2]
                             x_img = int(region["left"] + x + w // 2)
                             y_img = int(region["top"] + y + h // 2)
-                            print_padao(texto_1=f'Imagem {self.palv} encontra na tela {palv} valores X: {x_img} e Y:{y_img}')
+                            print_padao(texto_1=f'Imagem {palv} encontra na img {palv} valores X: {x_img} e Y:{y_img}')
                             return x_img, y_img
-                        salve_tela(arq_find, palv+'_nao_encontrado')
+                        salve_img(arq_find, palv+'_nao_encontrado')
                     chave, valor = next(iter(dict_arqs_find.items()))
-                    salve_tela(valor, chave+'_nao_encontrado')
-                elif self.metodo == "pyautogui":
-                    arq = resource_path(os.path.join('img', self.palv))
-                    box = list(pyautogui.locateAllOnScreen(arq, confidence=confidence, region=region_list))
+                    salve_img(valor, chave+'_nao_encontrado')
+                elif metodo == "pyautogui":
+                    arq = resource_path(os.path.join('img', palv))
+                    box = list(pyautogui.locateAllOnScreen(arq, confidence=confidence, region=region["list"]))
                     if box:
                         ponto = pyautogui.center(box[0])
                         return ponto.x, ponto.y
                 else:
-                    raise Exception(f'Valor inválido do método {self.metodo}.')
+                    raise Exception(f'Valor inválido do método {metodo}.')
                 # --- FLUXO DE FALHA DE BUSCA (Roda se não deu return no if acima) ---
-                confidence -= self.reduce_confidence
+                confidence -= reduce_confidence
                 confidence = round(confidence, 2)
                 if confidence <= confidence_print:
-                    print(f'\rImg: {self.palv} | Confid: {confidence}', end='')
+                    print(f'\rImg: {palv} | Confid: {confidence}', end='')
                     confidence_print -= 0.1
                 if confidence <= confidence_minima:
                     if count >= n_tentativas:
@@ -213,20 +252,21 @@ class ImageManip:
                     confidence_print = 1
                 sleep(0.05)     # Pausa no fluxo normal para liberar CPU
             except NameError as e:
-                print(f'\rImg: {self.palv} não existe | Erro: ({type(e).__name__}).', end='')
+                print(f'\rImg: {palv} não existe | Erro: ({type(e).__name__}).', end='')
                 return None, None
             except Exception as e:
                 print(f'\nErro inesperado: {type(e).__name__} - {e}')
                 return None, None
 
-
 class Palvclker:
-    def get_todos_dados(self, list_psm=[3, 6, 11, 12],  opcao='all', conf_min=0, limit_caracter=2, scale=2):
+    def get_todos_dados(self, list_psm=[3, 6, 11, 12], opcao='all', conf_min=0, limit_caracter=2, scale=2, width=None, height=None, index_nonitor=1, get_img='mss'):
         setup_tesseract()
-        region, region_list = define_region()
-        dict_arqs = get_tela_all(scale, region)
+        region = get_monitor(width, height, index_nonitor)
+        region['scale'] = scale
+        region['get_img'] = get_img
+        dict_arqs = get_all_img(region)
         for nome_arq in dict_arqs:
-            salve_tela(dict_arqs[nome_arq], nome_arq)
+            salve_img(dict_arqs[nome_arq], nome_arq)
         dict_imgs_verification = {opcao: dict_arqs[opcao]} if opcao in dict_arqs else dict_arqs
         dict_dados = {}
         for nome_img, img_tst in dict_imgs_verification.items():
@@ -251,10 +291,10 @@ class Palvclker:
                     w = dados['width'][i]
                     h = dados['height'][i]
                     if nome_img != "img" and nome_img != "img_cv":
-                        x = int(x / scale)
-                        y = int(y / scale)
-                        w = int(w / scale)
-                        h = int(h / scale)
+                        x = int(x / region['scale'])
+                        y = int(y / region['scale'])
+                        w = int(w / region['scale'])
+                        h = int(h / region['scale'])
                     # somar com o limite enviado para posicao correta n oregion
                     if region:
                         x += region['left']
@@ -278,33 +318,37 @@ class Palvclker:
             print('', end=' | ')
         return dict_dados if dict_dados else None
 
-
 if __name__ == '__main__':
-    palv = 'google'
-    palv = 'gmail.png'
-    list_psm = [11]
-    confidence = 0.1
+    # dafini e vai salvar as imagens em img\capturada
     salvar_imagens = True
-    region, region_list = define_region()
-    imageManip = ImageManip()
+
+    # procurar imagem
+    palv = 'gmail.png'
     print_padao(titulo=f'Pasta local: {palv}')
-    imageManip.palv = palv
-    imageManip.reduce_confidence = confidence
-    x, y = imageManip.locate_x_y
+    imageManip = ImageManip()
+
+    # imageManip.palv = palv
+    # imageManip.reduce_confidence = 0.1
+    x, y = imageManip.locate_x_y(palv=palv, reduce_confidence=0.1, width=1360, height=768)
     if not x or not y:
         print_padao(texto_1='x e y não existe')
     else:                                 # <--- RETORNA FALSE SE NÃO ACHAR
         print_padao(texto_1=f'X: {x} e Y: {y}')
-        print_padao(rodape=True)
-        print(x, y)
         clk_x_y(x, y)
+    print_padao(rodape=True)
+
+    # procurar palavra
     palvclker = Palvclker()
-    dados = palvclker.get_todos_dados(list_psm=list_psm, opcao='grayClr', limit_caracter=2)
-    print_padao(titulo=f'Palavra que sera porcurado é: {palv}')
+    palv = 'acotes'
+    print_padao(titulo=f'Palavra a porcura: {palv}')
+    dados = palvclker.get_todos_dados(list_psm=[11], opcao='contrEsc3', width=1360, height=768, get_img='pil', limit_caracter=2)
     list_palv = []
     encontrado = False
     for key_1, value_2 in dados.items():
-        key_palavra, _ = key_1.split("_|")
+        try:
+            key_palavra, _ = key_1.split("_|")
+        except ValueError:
+            continue
         if key_palavra not in list_palv:
             list_palv.append(key_palavra)
         if '_|' in palv:
@@ -317,18 +361,20 @@ if __name__ == '__main__':
                 print_padao(texto_1=key_1, texto_2=value_2)
                 encontrado = True
     if not encontrado:
-        print_padao(texto_1=f'\nPalavra {palv} não encontrada na tela, lista: {list_palv}.')
+        print_padao(texto_1=f'\nNão encontrada, lista:\n{list_palv}.')
     print_padao(rodape=True)
+
+    # palavra perto da posição do mouse
     x, y = pyautogui.position()
-    print_padao(titulo=f'Possição do mouse : X: {x}, Y: {y}. Palavra(s) próxima(s):')
+    print_padao(titulo=f'Possição do mouse : X: {x}, Y: {y}. Palavra(s) próxima(s) a direitra do mouse:')
     for key_1, value_2 in dados.items():
-        if not value_2['x']  <= x + 30:
+        if not value_2['x']  <= x + 100:
             continue
-        if not value_2['x']  >= x - 30:
+        if not value_2['x']  >= x:
             continue
-        if not value_2['y']  <= y + 50:
+        if not value_2['y']  <= y + 10:
             continue
-        if not value_2['y']  >= y - 50:
+        if not value_2['y']  >= y - 10:
             continue
         print_padao(texto_1=key_1, texto_2=value_2)
     print_padao(rodape=True)
